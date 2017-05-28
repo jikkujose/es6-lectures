09e73d656b4faf2dff6a1d4661c9633841e0894c
Jikku Jose <jikkujose@gmail.com>
2017-05-28 16:36:39 +0530

    Add recommended structure

---
+++ sample-data/recommended.js
@@ -0,0 +1,22 @@
+const good = {
+  "id": 1,
+  "name": "Amal Shehu",
+  "date": "2017-5-27",
+  "gems": [{
+    "count": 5,
+    "name": "Ruby",
+    "code": "GEM001"
+  }]
+}
+
+const better = {
+  "id": 1,
+  "name": "Amal Shehu",
+  "date": "2017-5-27",
+  "gems": [{
+    "GEM001": {
+      "count": 5,
+      "name": "Ruby",
+    }
+  }]
+}
